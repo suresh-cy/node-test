c27f8425a0cdf1e52f242026d467893f565d89c3
suresh-cy <128201808+suresh-cy@users.noreply.github.com>
2023-04-03 10:10:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 var port=process.env.PORT;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-alert(port)
+console.log(port)
   res.end('Hello World OK IT WORKSSSSS!');
 }).listen(port);
