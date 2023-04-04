d50b1c62e9bd7fad83751ed7f85b060cc4be5001
suresh-cy <128201808+suresh-cy@users.noreply.github.com>
2023-04-04 19:43:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express= require('express');
 var app=express();
 var port=process.env.PORT;
-
+var url = require('url');
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
@@ -9,7 +9,7 @@ var con = mysql.createConnection({
     port: "10072",
     user: "admin",
     password: "h82xVHw7",
-    database:"node-test"
+    database:"nodetest"
     
   });
   con.connect(function(err) {
@@ -21,24 +21,35 @@ var con = mysql.createConnection({
 
 
 
-app.get('/',function (req, res) {
-  res.header("Access-Control-Allow-Origin","*");
+app.get('/',function(req,res){
+    res.header("Access-Control-Allow-Origin","*")
+   
+    var q=url.parse(req.url,true).query;
+var txt=q.name1;
+var name1=["'",txt,"'"]
+var txt2=q.name2;
+var txt3=q.result;
+var name2=["'",txt2,"'"]
+var result=["'",txt3,"'"]
+console.log(name1,name2,result)
+console.log(txt,txt2)
+   // if(fl!=undefined){
 
-  res.send('Hello World OK IT WORKSSSSS!');
-
-var ss='INSERT INTO flames (name1,name2,result ) VALUES ("suresh","ram","nothing")';
-  con.query(ss,function(err,result,fields){
-    if(err){
-        console.log(err)
-      }else{
-        console.log(result)
-          console.log(result)
-      }
-  })
-
-
-
+        var ss='INSERT INTO flames (name1,name2,result ) VALUES ('+name1.join('')+','+name2.join('')+','+result.join('')+')';
+      
+      
+      con.query(ss,function(err,result,fields){
+          if(err){
+            console.log(err)
+          }else{
+            console.log(result)
+              res.send(result)
+          }
+      
+      })
+     // }
 })
+
 
 app.listen(port,()=>{
 console.log("connected at port")
